0b298389a8b3599cbf762df0b1ff79a866838309
Anuradha Mudhale <anuradhamudhale314@gmail.com>
2025-12-10 17:17:35 +0530

    geometry

--- Main.py
+++ Main.py
@@ -1,9 +1,14 @@
 import Arithmetic 
+import geometry as geo
 print("Hello World!!!!!")
 
 a = int(input("Enter num1"))
 b = int(input("Enter num2"))
+l = int(input("Enter length"))
 
 Arithmetic.add(a,b)
 Arithmetic.sub(a,b)
 Arithmetic.mul(a,b)
+
+geo.areaofrect(l,b)
+geo.perimeterofrect(l,b)
---
+++ geometry.py
@@ -0,0 +1,5 @@
+def areaofrect(l,b):
+    print("area of rectangle is",l*b)
+
+def perimeterofrect(l,b):
+    print("perimeter of rectangle is:",2*(l+b))
