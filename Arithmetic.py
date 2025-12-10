809d4af7c3f98a29daf3cdda17e4b8f294e1b7af
Anuradha Mudhale <anuradhamudhale314@gmail.com>
2025-12-10 16:57:24 +0530

    add substract

---
+++ Arithmetic.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    print("addition is:",a+b)
+
+def sub(a,b):
+    print("substraction is:",a-b)
+
+def mul(a,b):
+    print("Multiplication is:",a*b)
--- Main.py
+++ Main.py
@@ -1,1 +1,9 @@
+import Arithmetic 
 print("Hello World!!!!!")
+
+a = int(input("Enter num1"))
+b = int(input("Enter num2"))
+
+Arithmetic.add(a,b)
+Arithmetic.sub(a,b)
+Arithmetic.mul(a,b)
